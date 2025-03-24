fff925e951dfd28b36616f117b14675ad8e5f69d
妖雨纯 <876451336@qq.com>
2025-03-24 12:26:27 +0800

    ✨ feat(unocss): 新增基础通用样式

--- uno.config.ts
+++ uno.config.ts
@@ -6,6 +6,22 @@ import presetAttributify from '@unocss/preset-attributify'
 
 export default defineConfig({
     presets: [presetUno(), presetAttributify(), presetTagify(), presetRemToPx({ baseFontSize: 4 })],
-    rules: [],
+    rules: [
+        ['text-naive-primary', { color: 'var(--primary-color)', transition: 'color .3s var(--cubic-bezier-ease-in-out)' }],
+        ['text-naive-success', { color: 'var(--success-color)', transition: 'color .3s var(--cubic-bezier-ease-in-out)' }],
+        ['text-naive-warning', { color: 'var(--warning-color)', transition: 'color .3s var(--cubic-bezier-ease-in-out)' }],
+        ['text-naive-error', { color: 'var(--error-color)', transition: 'color .3s var(--cubic-bezier-ease-in-out)' }],
+        ['text-primary', { color: 'var(--text-color-1)', transition: 'color .3s var(--cubic-bezier-ease-in-out)' }],
+        ['text-secondary', { color: 'var(--text-color-2)', transition: 'color .3s var(--cubic-bezier-ease-in-out)' }],
+        ['text-regular', { color: 'var(--text-color-3)', transition: 'color .3s var(--cubic-bezier-ease-in-out)' }],
+        ['text-placeholder', { color: 'var(--placeholder-color)', transition: 'color .3s var(--cubic-bezier-ease-in-out)' }],
+        ['text-disabled', { color: 'var(--placeholder-color-disabled)', transition: 'color .3s var(--cubic-bezier-ease-in-out)' }],
+        ['text-transition', { transition: 'color .3s var(--cubic-bezier-ease-in-out)' }],
+        ['bg-primary', { 'background-color': 'var(--primary-color)', transition: 'background-color 0.3s var(--cubic-bezier-ease-in-out)' }],
+        ['bg-success', { 'background-color': 'var(--success-color)', transition: 'background-color 0.3s var(--cubic-bezier-ease-in-out)' }],
+        ['bg-warning', { 'background-color': 'var(--warning-color)', transition: 'background-color 0.3s var(--cubic-bezier-ease-in-out)' }],
+        ['bg-error', { 'background-color': 'var(--error-color)', transition: 'background-color 0.3s var(--cubic-bezier-ease-in-out)' }],
+        ['bg-transition', { transition: 'background-color 0.3s var(--cubic-bezier-ease-in-out)' }]
+    ],
     shortcuts: [{ 'border-divider': 'border border-solid border-[var(--divider-color)]' }]
 })

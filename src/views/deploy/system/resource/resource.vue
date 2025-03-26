497f9888128d4c97bafb0da60f0cc238ecc1f5da
妖雨纯 <876451336@qq.com>
2025-03-26 22:10:01 +0800

    ✨ feat(综合设置 - 系统管理 - 资源管理): 修改表格布局

--- src/views/deploy/system/resource/resource.vue
+++ src/views/deploy/system/resource/resource.vue
@@ -9,16 +9,16 @@ export default defineComponent({
         const { state } = useColumnService({
             form: {},
             columns: [
-                { title: '菜单名称', key: 'name', width: 200, fixed: 'left' },
-                { title: '图标', key: 'iconName', width: 100, align: 'center' },
-                { title: '类型', key: 'type', width: 120, align: 'center' },
+                { title: '菜单名称', key: 'name', width: 200 },
+                { title: '图标', key: 'iconName', width: 80, align: 'center' },
+                { title: '类型', key: 'type', width: 100, align: 'center' },
                 { title: '路由地址', key: 'router', minWidth: 300 },
                 { title: '权限标识', key: 'key', minWidth: 300 },
-                { title: '排序号', key: 'sort', width: 100, align: 'center' },
-                { title: '状态', key: 'status', width: 120, align: 'center' },
+                { title: '排序号', key: 'sort', width: 80, align: 'center' },
+                { title: '状态', key: 'status', width: 100, align: 'center' },
                 { title: '更新时间', key: 'modifyTime', width: 200, align: 'center' },
-                { title: '更新人', key: 'user', width: 140, align: 'center' },
-                { title: '操作', key: 'command', width: 180, align: 'center', fixed: 'right' }
+                { title: '更新人', key: 'user', width: 120, align: 'center' },
+                { title: '操作', key: 'command', width: 150, align: 'center' }
             ],
             request: (data, base) => Service.httpBaseColumnSystemRouter({ page: base.page, size: base.size })
         })
@@ -28,21 +28,23 @@ export default defineComponent({
                 <div class="flex items-center justify-end">
                     <common-search-action></common-search-action>
                 </div>
-                <common-data-table bordered striped single-line={false} scroll-x={1800} columns={state.columns} data={state.dataSource}>
-                    {{
-                        iconName: (data: Omix) => (
-                            <div class="flex items-center justify-center">
-                                <common-icon class="flex-inline" size={28} name={data.iconName}></common-icon>
-                            </div>
-                        ),
-                        type: (data: Omix) => (
-                            <n-tag bordered={false} type="success">
-                                312
-                            </n-tag>
-                        ),
-                        user: (data: Omix) => data.user.nickname
-                    }}
-                </common-data-table>
+                <div class="flex-1 overflow-hidden">
+                    <common-data-table bordered striped single-line={false} scroll-x={1600} columns={state.columns} data={state.dataSource}>
+                        {{
+                            iconName: (data: Omix) => (
+                                <div class="flex items-center justify-center">
+                                    <common-icon class="flex-inline" size={28} name={data.iconName}></common-icon>
+                                </div>
+                            ),
+                            type: (data: Omix) => (
+                                <n-tag bordered={false} type="success">
+                                    312
+                                </n-tag>
+                            ),
+                            user: (data: Omix) => data.user.name
+                        }}
+                    </common-data-table>
+                </div>
             </layout-common-container>
         )
     }

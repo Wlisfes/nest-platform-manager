394e3c7784ae557a0d8fec9b57495db5966ee386
妖雨纯 <876451336@qq.com>
2025-03-27 00:13:19 +0800

    ✨ feat(components/common): 修改高级筛选组件

--- src/components/common/common-search-action.vue
+++ src/components/common/common-search-action.vue
@@ -1,110 +1,114 @@
 <script lang="tsx">
-import { defineComponent, ref, PropType } from 'vue'
-import { FormProps, GridProps, PopoverInst } from 'naive-ui'
+import { defineComponent, ref, Ref } from 'vue'
+import { PopoverInst } from 'naive-ui'
 import { useVModels } from '@vueuse/core'
-import { useState } from '@/hooks/hook-state'
-import { fetchWhere, enter, stop } from '@/utils/utils-common'
+import { enter } from '@/utils/utils-common'
 
 export default defineComponent({
     name: 'CommonSearchAction',
     emits: ['submit', 'update:keyword'],
     props: {
-        keyword: { type: String },
-        scale: { type: Number, default: 0.8 },
-        distance: { type: Number, default: 200 },
-        maxWidth: { type: Number, default: 450 },
-        itemWidth: { type: Number, default: 456 },
-        multiple: { type: Boolean, default: false },
+        /**排列布局**/
+        cols: { type: Number, default: 1 },
+        /**弹出层宽度**/
+        width: { type: String, default: '450px' },
+        /**弹出层最大高度**/
+        maxHeight: { type: String, default: '450px' },
+        /**输入框最大宽度**/
+        maxWidth: { type: Number, default: 400 },
+        /**模糊查询关键字**/
+        vague: { type: String },
+        /**搜索框占位符**/
         placeholder: { type: String },
-        form: { type: Object as PropType<Omix>, default: () => ({}) },
-        formProps: { type: Object as PropType<FormProps>, default: () => ({}) },
-        gridProps: { type: Object as PropType<GridProps>, default: () => ({}) }
+        /**字符占位宽度**/
+        labelWidth: { type: String, default: 'auto' }
     },
     setup(props, { emit, slots }) {
-        const instance = ref<PopoverInst>()
-        const { keyword } = useVModels(props, emit)
-        const { state, setState } = useState({
-            pattern: undefined,
-            cols: 1,
-            width: `${props.itemWidth}px`,
-            height: (window.innerHeight - props.distance) * fetchWhere(props.scale > 0.8, 0.8, props.scale) + 'px'
-        })
+        const instance = ref() as Ref<PopoverInst>
+        const { vague } = useVModels(props, emit)
 
-        /**弹出层开启计算**/
-        async function fetchUpdate(value: boolean) {
-            if (props.multiple && window.innerWidth > props.itemWidth * 2 + 68) {
-                return await setState({ cols: 2, width: `${props.itemWidth * 2 + 12}px` })
-            }
-            return await setState({ cols: 1, width: `${props.itemWidth}px` })
-        }
-
+        /**输入框回车事件**/
         async function fetchKeyup(evt: KeyboardEvent) {
             return enter(evt, async () => {
                 return await fetchEvent('input-submit')
             })
         }
 
-        async function fetchClick(evt: MouseEvent) {
-            return stop(evt, async () => {
-                return await fetchEvent('input-submit')
-            })
-        }
-
+        /**事件触发**/
         async function fetchEvent(eventName: 'input-submit' | 'submit' | 'refresh') {
-            return await emit('submit', { event: eventName, pattern: state.pattern, form: props.form })
+            return await emit('submit', { event: eventName })
         }
 
+        /**确定表达提交**/
         async function fetchSubmit(evt: MouseEvent) {
             return await fetchEvent('submit').then(() => {
-                return instance.value!.setShow(false)
+                return instance.value.setShow(false)
             })
         }
 
         return () => (
-            <div class="common-search-action w-full flex gap-10" style={{ maxWidth: props.maxWidth + 'px' }}>
-                <n-input class="flex-1" v-model:value={keyword.value} placeholder={props.placeholder} onKeyup={fetchKeyup}>
-                    {{
-                        prefix: () => (
-                            <n-icon
-                                class="cursor-pointer hover:text-[var(--n-text-color)]"
-                                size={18}
-                                component={<local-naive-search />}
-                                onClick={fetchClick}
-                            ></n-icon>
-                        )
-                    }}
-                </n-input>
-                <n-popover
-                    ref={instance}
-                    placement="bottom"
-                    trigger="click"
-                    arrow-point-to-center
-                    style={{ padding: 0 }}
-                    on-update:show={fetchUpdate}
+            <div class="common-search-action flex flex-1 gap-10">
+                <n-input
+                    v-model:value={vague.value}
+                    placeholder={props.placeholder}
+                    onKeyup={fetchKeyup}
+                    style={{ maxWidth: props.maxWidth + 'px' }}
                 >
+                    {{ prefix: () => <n-icon size={18} component={<local-nest-search />}></n-icon> }}
+                </n-input>
+                <n-popover ref={instance} placement="bottom" trigger="click" style={{ padding: 0 }}>
                     {{
                         trigger: () => (
-                            <common-state-button icon={<local-naive-filter />}>
+                            <n-button focusable={false} v-slots={{ icon: () => <local-nest-filter /> }}>
                                 <span>筛选</span>
-                            </common-state-button>
+                            </n-button>
                         ),
                         default: () => (
-                            <n-element class="flex flex-col">
-                                <n-h1 class="m-0 text-16 line-height-24 p-inline-16 p-block-10">高级筛选</n-h1>
-                                <n-scrollbar style={{ maxHeight: state.height }}>
-                                    <n-form
-                                        class="p-inline-16"
-                                        {...{ labelWidth: 'auto', labelPlacement: 'left', showFeedback: false, ...props.formProps }}
-                                    >
-                                        <n-grid
-                                            style={{ width: state.width }}
-                                            {...{ xGap: 12, yGap: 12, cols: state.cols, ...props.gridProps }}
-                                        >
-                                            {slots.default && slots.default()}
+                            <n-element class="flex flex-col" style={{ width: props.width }}>
+                                <n-h1 class="m-0 text-16 line-height-24 p-inline-14 p-block-10">高级筛选</n-h1>
+                                <n-scrollbar style={{ maxHeight: props.maxHeight }}>
+                                    <n-form class="p-inline-14" label-width={props.labelWidth} label-placement="left" show-feedback={false}>
+                                        <n-grid cols={props.cols >= 2 ? 2 : props.cols} y-gap={12} x-gap={12}>
+                                            <n-form-item-gi span={1} label="消费用户导入" path="user.name">
+                                                <n-input placeholder="输入姓名" />
+                                            </n-form-item-gi>
+                                            <n-form-item-gi label="姓名" path="user.name">
+                                                <n-input placeholder="输入姓名" />
+                                            </n-form-item-gi>
+                                            <n-form-item-gi label="姓名" path="user.name">
+                                                <n-input placeholder="输入姓名" />
+                                            </n-form-item-gi>
+                                            <n-form-item-gi label="姓名" path="user.name">
+                                                <n-input placeholder="输入姓名" />
+                                            </n-form-item-gi>
+                                            <n-form-item-gi label="姓名" path="user.name">
+                                                <n-input placeholder="输入姓名" />
+                                            </n-form-item-gi>
+                                            <n-form-item-gi label="姓名" path="user.name">
+                                                <n-input placeholder="输入姓名" />
+                                            </n-form-item-gi>
+                                            <n-form-item-gi label="姓名" path="user.name">
+                                                <n-input placeholder="输入姓名" />
+                                            </n-form-item-gi>
+                                            <n-form-item-gi label="姓名" path="user.name">
+                                                <n-input placeholder="输入姓名" />
+                                            </n-form-item-gi>
+                                            <n-form-item-gi label="姓名" path="user.name">
+                                                <n-input placeholder="输入姓名" />
+                                            </n-form-item-gi>
+                                            <n-form-item-gi label="姓名" path="user.name">
+                                                <n-input placeholder="输入姓名" />
+                                            </n-form-item-gi>
+                                            <n-form-item-gi label="姓名" path="user.name">
+                                                <n-input placeholder="输入姓名" />
+                                            </n-form-item-gi>
+                                            <n-form-item-gi span={2} label="姓名" path="user.name">
+                                                <n-date-picker type="datetimerange" clearable default-time="13:22:11" class="w-full" />
+                                            </n-form-item-gi>
                                         </n-grid>
                                     </n-form>
                                 </n-scrollbar>
-                                <n-flex justify="center" class="p-inline-16 p-block-14">
+                                <n-flex justify="center" class="p-inline-14 p-block-10">
                                     <n-button class="min-w-80">重置</n-button>
                                     <n-button class="min-w-80" type="primary" onClick={fetchSubmit}>
                                         确定
@@ -114,7 +118,6 @@ export default defineComponent({
                         )
                     }}
                 </n-popover>
-                {/* <common-state-button circle icon={<local-naive-refresh />} onClick={fetchRefresh}></common-state-button> */}
             </div>
         )
     }

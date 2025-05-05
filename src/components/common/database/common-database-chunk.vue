0ac1602200719907668c1da3f0f80d91d4b354d2
妖雨纯 <876451336@qq.com>
2025-05-05 17:54:40 +0800

    ✨ feat(综合设置 - 系统管理 - 用户管理): 完善列表展示布局

--- src/components/common/database/common-database-chunk.vue
+++ src/components/common/database/common-database-chunk.vue
@@ -7,11 +7,13 @@ export default defineComponent({
     inheritAttrs: false,
     props: {
         /**渲染类型**/
-        element: { type: String as PropType<'chunk' | 'text' | 'icon'>, default: 'chunk' },
+        element: { type: String as PropType<'chunk' | 'text' | 'icon' | 'avatar'>, default: 'chunk' },
         /**icon图标渲染**/
         iconSize: { type: Number, default: 0 },
         /**文本内容**/
         content: { type: [String, Number] },
+        /**头像地址**/
+        url: { type: String },
         /**标签边框**/
         bordered: { type: Boolean, default: false },
         /**标签配置**/
@@ -23,7 +25,15 @@ export default defineComponent({
                 {slots.default ? (
                     slots.default(props.content, props.json)
                 ) : props.element === 'text' ? (
-                    <span class="common-database-chunk-text">{isEmpty(props.content) ? '-' : props.content}</span>
+                    <div class="common-database-chunk-text overflow-hidden">
+                        {isEmpty(props.content) ? (
+                            '-'
+                        ) : (
+                            <n-ellipsis tooltip={{ scrollable: true, style: { maxWidth: '540px', maxHeight: '540px' } }}>
+                                {props.content}
+                            </n-ellipsis>
+                        )}
+                    </div>
                 ) : props.element === 'icon' ? (
                     <div class="common-database-chunk-icon flex items-center justify-center">
                         {isEmpty(props.content) ? (
@@ -33,6 +43,16 @@ export default defineComponent({
                                 text
                                 icon={<common-element-icon size={props.iconSize} name={props.content}></common-element-icon>}
                             ></common-element-button>
+                        )}
+                    </div>
+                ) : props.element === 'avatar' ? (
+                    <div class="common-database-chunk-avatar flex items-center justify-center">
+                        {isEmpty(props.url) ? (
+                            <n-avatar size="medium" color="var(--primary-color)">
+                                {String(props.content ?? '-').slice(0, 1)}
+                            </n-avatar>
+                        ) : (
+                            <n-avatar size="medium" src={props.url}></n-avatar>
                         )}
                     </div>
                 ) : (
--- src/views/deploy/system/user/user.vue
+++ src/views/deploy/system/user/user.vue
@@ -1,12 +1,92 @@
 <script lang="tsx">
 import { defineComponent } from 'vue'
+import { useColumnService, fetchKineColumns } from '@/hooks/hook-service'
+import { fetchDialogService, fetchNotifyService } from '@/plugins'
+import * as feedback from '@/components/deploy/hooks'
+import * as Service from '@/api/instance.service'
 
 export default defineComponent({
     name: 'DeploySystemUser',
     setup(props, ctx) {
+        const { root, state, form, full, setState, toggle, fetchCheckboxs, fetchRefresh } = useColumnService({
+            request: (data, base, opts) => Service.httpBaseSystemColumnUser(opts.body),
+            document: '用户管理自定义表头',
+            dynamic: 'base:deploy:system:user',
+            form: {
+                vague: undefined
+            },
+            columns: fetchKineColumns(true, [
+                { title: 'UID', key: 'uid', width: 190 },
+                { title: '头像', key: 'avatar', width: 80, align: 'center', check: true },
+                { title: '名称', key: 'name', width: 120, check: true },
+                { title: '邮箱', key: 'email', width: 200, check: true },
+                { title: '手机号', key: 'phone', width: 150, check: true },
+                { title: '归属部门', key: 'depts', width: 150, check: true },
+                { title: '已关联角色', key: 'roles', width: 150, check: true },
+                { title: '状态', key: 'statusChunk', width: 100, align: 'center', check: true },
+                { title: '入职时间', key: 'createTime', width: 170, check: true }
+            ])
+        })
+
         return () => (
-            <layout-common-container class="absolute inset-0 p-12" class-name="p-12 gap-12 overflow-hidden">
-                <common-element class="flex-1">用户管理</common-element>
+            <layout-common-container ref={root} abstract class="absolute inset-0 p-12" class-name="p-12 gap-12 overflow-hidden">
+                <common-database-compute
+                    element-class="flex-row-reverse justify-between"
+                    keys={['refresh', 'settings', 'size', 'full']}
+                    v-model:full={full.value}
+                    v-model:loading={state.loading}
+                    v-model:columns={state.columns}
+                    toggle={toggle}
+                    onRefresh={fetchRefresh}
+                    onCheckboxs={fetchCheckboxs}
+                >
+                    <n-element class="flex gap-10 items-center"></n-element>
+                    <common-element-action
+                        label-width="7.2em"
+                        cols={2}
+                        v-model:initialize={state.initialize}
+                        v-model:loading={state.loading}
+                        v-model:event={state.event}
+                        v-model:vague={form.value.vague}
+                        onSubmit={() => fetchRefresh()}
+                    ></common-element-action>
+                </common-database-compute>
+                <common-database-container element-table v-model:initialize={state.initialize} v-model:loading={state.loading}>
+                    <common-database-table
+                        pagination
+                        loading={state.loading}
+                        data={state.dataSource}
+                        v-model:columns={state.columns}
+                        v-model:page={state.page}
+                        v-model:size={state.size}
+                        v-model:total={state.total}
+                        v-model:row-keys={state.rowKeys}
+                        v-model:row-nodes={state.rowNodes}
+                        onUpdate:page={(page: number) => fetchRefresh()}
+                        onUpdate:size={(size: number) => fetchRefresh({ page: 1 })}
+                    >
+                        {{
+                            avatar: (url: Omix, node: Omix) => {
+                                return <common-database-chunk element="avatar" url={url} content={node.name}></common-database-chunk>
+                            },
+                            statusChunk: (data: Omix) => {
+                                return <common-database-chunk content={data.name} json={data.json}></common-database-chunk>
+                            },
+                            depts: (items: Array<Omix>) => (
+                                <common-database-chunk
+                                    element="text"
+                                    content={items.map(k => k.deptNode.name).join('、')}
+                                ></common-database-chunk>
+                            ),
+                            roles: (items: Array<Omix>) => (
+                                <common-database-chunk
+                                    element="text"
+                                    content={items.map(k => k.roleNode.name).join('、')}
+                                ></common-database-chunk>
+                            )
+                        }}
+                    </common-database-table>
+                </common-database-container>
             </layout-common-container>
         )
     }

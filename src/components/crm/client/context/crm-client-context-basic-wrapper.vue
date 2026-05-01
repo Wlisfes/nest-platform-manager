91e4ff42f584c5eb83fe425d4adeeae327067bfc
妖雨纯 <876451336@qq.com>
2026-05-01 19:53:31 +0800

    ♻️ refactor(CRM - 客户管理): 客户详情基本信息组件重构,使用直接渲染替代computed数组映射
    - 移除computed导入和columns计算属性
    - 将15个字段从数组映射改为直接JSX渲染
    - 统一使用?? '-'处理空值显示
    - 删除map循环,提升渲染性能和代码可读性

--- src/components/crm/client/context/crm-client-context-basic-wrapper.vue
+++ src/components/crm/client/context/crm-client-context-basic-wrapper.vue
@@ -1,5 +1,5 @@
 <script lang="tsx">
-import { defineComponent, computed, PropType } from 'vue'
+import { defineComponent, PropType } from 'vue'
 import { useVModels } from '@vueuse/core'
 
 export default defineComponent({
@@ -16,87 +16,80 @@ export default defineComponent({
     setup(props, { emit }) {
         const { faseNode } = useVModels(props, emit)
 
-        const columns = computed(() => {
-            return [
-                { label: '客户名称：', vague: faseNode.value.name },
-                {
-                    label: '客户类型：',
-                    render: () => (
-                        <common-database-table-chunk
-                            element="content"
-                            value={faseNode.value.classType}
-                            options={props.chunkState.CHUNK_CLIENT_CLASS}
-                        ></common-database-table-chunk>
-                    )
-                },
-                { label: '品牌：', vague: faseNode.value.brandOptions?.name },
-                { label: '邮箱：', vague: faseNode.value.email },
-                { label: '电话号码：', vague: faseNode.value.phone },
-                { label: '客户别名：', vague: faseNode.value.alias },
-                {
-                    label: '归属人：',
-                    render: () => (
-                        <common-database-table-user element="text" data={faseNode.value.accountOptions}></common-database-table-user>
-                    )
-                },
-                { label: '归属部门：', vague: faseNode.value.deptOptions?.deptName },
-                { label: '当前等级：', vague: faseNode.value.level },
-                {
-                    label: '当前阶段：',
-                    render: () => (
-                        <common-database-table-chunk
-                            element="content"
-                            value={faseNode.value.stage}
-                            options={props.chunkState.CHUNK_CLIENT_STAGE}
-                        ></common-database-table-chunk>
-                    )
-                },
-                { label: '币种：', vague: faseNode.value.currency },
-                {
-                    label: '认证状态：',
-                    render: () => (
-                        <common-database-table-chunk
-                            element="content"
-                            value={faseNode.value.authStatus}
-                            options={props.chunkState.CHUNK_CLIENT_AUTH_STATUS}
-                        ></common-database-table-chunk>
-                    )
-                },
-                {
-                    label: '注册来源：',
-                    render: () => (
-                        <common-database-table-chunk
-                            element="content"
-                            value={faseNode.value.source}
-                            options={props.chunkState.CHUNK_CLIENT_SOURCE}
-                        ></common-database-table-chunk>
-                    )
-                },
-                {
-                    label: '付款模式：',
-                    render: () => (
-                        <common-database-table-chunk
-                            element="content"
-                            value={faseNode.value.payMode}
-                            options={props.chunkState.CHUNK_CLIENT_PAY_MODE}
-                        ></common-database-table-chunk>
-                    )
-                },
-                { label: '信用额度：', vague: faseNode.value.credit }
-            ]
-        })
-
         return () => (
             <common-element-scrollbar class="crm-client-context-basic-wrapper">
                 <common-element is-white class="flex flex-col flex-1 p-14 overflow-hidden">
                     <div class="flex flex-col overflow-hidden">
                         <common-business-header bar title="基本信息"></common-business-header>
                         <common-element-columns-template class="gap-y-10 gap-x-20 p-inline-10 p-block-10" type="auto-fit" number={400}>
-                            {columns.value.map((col: Omix, index: number) => (
-                                <common-element-columns-wrapper key={index} label-class={props.labelClass} label={col.label}>
-                                    {col.render ? col.render() : (col.vague ?? '-')}
-                                </common-element-columns-wrapper>
-                            ))}
+                            <common-element-columns-wrapper label-class={props.labelClass} label="客户名称：">
+                                {faseNode.value.name ?? '-'}
+                            </common-element-columns-wrapper>
+                            <common-element-columns-wrapper label-class={props.labelClass} label="客户类型：">
+                                <common-database-table-chunk
+                                    element="content"
+                                    value={faseNode.value.classType}
+                                    options={props.chunkState.CHUNK_CLIENT_CLASS}
+                                ></common-database-table-chunk>
+                            </common-element-columns-wrapper>
+                            <common-element-columns-wrapper label-class={props.labelClass} label="品牌：">
+                                {faseNode.value.brandOptions?.name ?? '-'}
+                            </common-element-columns-wrapper>
+                            <common-element-columns-wrapper label-class={props.labelClass} label="邮箱：">
+                                {faseNode.value.email ?? '-'}
+                            </common-element-columns-wrapper>
+                            <common-element-columns-wrapper label-class={props.labelClass} label="电话号码：">
+                                {faseNode.value.phone ?? '-'}
+                            </common-element-columns-wrapper>
+                            <common-element-columns-wrapper label-class={props.labelClass} label="客户别名：">
+                                {faseNode.value.alias ?? '-'}
+                            </common-element-columns-wrapper>
+                            <common-element-columns-wrapper label-class={props.labelClass} label="归属人：">
+                                <common-database-table-user
+                                    element="text"
+                                    data={faseNode.value.accountOptions}
+                                ></common-database-table-user>
+                            </common-element-columns-wrapper>
+                            <common-element-columns-wrapper label-class={props.labelClass} label="归属部门：">
+                                {faseNode.value.deptOptions?.deptName ?? '-'}
+                            </common-element-columns-wrapper>
+                            <common-element-columns-wrapper label-class={props.labelClass} label="当前等级：">
+                                {faseNode.value.level ?? '-'}
+                            </common-element-columns-wrapper>
+                            <common-element-columns-wrapper label-class={props.labelClass} label="当前阶段：">
+                                <common-database-table-chunk
+                                    element="content"
+                                    value={faseNode.value.stage}
+                                    options={props.chunkState.CHUNK_CLIENT_STAGE}
+                                ></common-database-table-chunk>
+                            </common-element-columns-wrapper>
+                            <common-element-columns-wrapper label-class={props.labelClass} label="币种：">
+                                {faseNode.value.currency ?? '-'}
+                            </common-element-columns-wrapper>
+                            <common-element-columns-wrapper label-class={props.labelClass} label="认证状态：">
+                                <common-database-table-chunk
+                                    element="content"
+                                    value={faseNode.value.authStatus}
+                                    options={props.chunkState.CHUNK_CLIENT_AUTH_STATUS}
+                                ></common-database-table-chunk>
+                            </common-element-columns-wrapper>
+                            <common-element-columns-wrapper label-class={props.labelClass} label="注册来源：">
+                                <common-database-table-chunk
+                                    element="content"
+                                    value={faseNode.value.source}
+                                    options={props.chunkState.CHUNK_CLIENT_SOURCE}
+                                ></common-database-table-chunk>
+                            </common-element-columns-wrapper>
+                            <common-element-columns-wrapper label-class={props.labelClass} label="付款模式：">
+                                <common-database-table-chunk
+                                    element="content"
+                                    value={faseNode.value.payMode}
+                                    options={props.chunkState.CHUNK_CLIENT_PAY_MODE}
+                                ></common-database-table-chunk>
+                            </common-element-columns-wrapper>
+                            <common-element-columns-wrapper label-class={props.labelClass} label="信用额度：">
+                                {faseNode.value.credit ?? '-'}
+                            </common-element-columns-wrapper>
                         </common-element-columns-template>
                     </div>
                 </common-element>
